ba7b63ec1aef13a6da647f1e9eef361207565f6e
Victor Motta <victor.mottag@gmail.com>
2022-10-19 03:03:16 -0300

    Adicionando função de virar carta, e adicionando cartas ao js.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-alert("Hello world!");
+const primeiraCarta = document.querySelector(".card-bobrossparrot");
+const segundaCarta = document.querySelector(".card-explodyparrot");
+const terceiraCarta = document.querySelector(".card-fiestaparrot");
+const quartaCarta = document.querySelector(".card-metalparrot");
+const quintaCarta = document.querySelector(".card-revertitparrot");
+const sextaCarta = document.querySelector(".card-tripletsparrot");
+const setimaCarta = document.querySelector(".card-unicornparrot");
+let quantidadeCarta;
+
+console.log(primeiraCarta);
+function embaralhar() {}
+
+function viraCarta(virar) {
+    virar.classList.add("flip");
+}
