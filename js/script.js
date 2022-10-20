ac9cb05acb4db18b9d2923696ddb991a40904564
Victor Motta <victor.mottag@gmail.com>
2022-10-20 15:31:37 -0300

    Implementado as regras do jogo, jogo quase funcionando 100%

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let jogadaClass;
+
 let personagens = [
     "explodyparrot",
     "bobrossparrot",
@@ -10,7 +12,9 @@ let personagens = [
 
 let carta = [];
 let cartasDuplicadas = [];
-let qtdCartas = 0;
+let qtdPersonagens = 0;
+let jogadas = [];
+let pontuacao = 0;
 
 function escolheDificuldade() {
     let condition = true;
@@ -20,10 +24,9 @@ function escolheDificuldade() {
         );
         let par = escolha % 2;
         if (par == 0 && escolha >= 4 && escolha <= 14) {
-            qtdCartas = escolha / 2;
+            qtdPersonagens = escolha / 2;
             condition = false;
-            console.log(qtdCartas);
-            adicionaCartas();
+            console.log(qtdPersonagens);
             loadGame();
         } else {
             alert("Escolha um número entre 4 e 14 e somente número par: ");
@@ -48,7 +51,7 @@ function criaCartas(caracter) {
 }
 
 function adicionaCartas() {
-    for (let i = 0; i < qtdCartas; i++) {
+    for (let i = 0; i < qtdPersonagens; i++) {
         carta.push(personagens[i]);
     }
 }
@@ -56,11 +59,11 @@ function adicionaCartas() {
 function sorteiaCartas() {
     cartasDuplicadas = [...carta, ...carta];
     const cartasSorteadas = cartasDuplicadas.sort((a, b) => Math.random() - 0.5);
-
     return cartasSorteadas;
 }
 
 function loadGame() {
+    adicionaCartas();
     const cartasSorteadas = sorteiaCartas();
 
     cartasSorteadas.forEach((caracter) => {
@@ -68,8 +71,65 @@ function loadGame() {
     });
 }
 
+function regrasDoJogo(cartaVirada) {
+    jogadas.push(cartaVirada);
+    jogadaClass = document.querySelectorAll(".jogada");
+    console.log(jogadaClass);
+
+    if (jogadas[1] === undefined) {
+        return;
+    } else if (jogadas.length > 2) {
+        for (let i = 0; i < jogadas.length; i++) {
+            jogadas[i].classList.remove("jogada");
+            jogadas[i].classList.remove("flip");
+            if (jogadas[i] === undefined) {
+                return;
+            }
+        }
+        jogadas = [];
+    } else if (jogadas[0].id === jogadas[1].id) {
+        jogadaClass[0].classList.remove("jogada");
+        jogadaClass[1].classList.remove("jogada");
+        jogadaClass[0].classList.add("acertada");
+        jogadaClass[1].classList.add("acertada");
+        pontuacao++;
+        jogadas = [];
+        console.log("Entrou no if");
+    } else {
+        setTimeout(function () {
+            if (jogadaClass[1] === undefined) {
+                return;
+            }
+            if (
+                jogadaClass[0].classList.contains("jogada") &&
+                jogadaClass[1].classList.contains("jogada")
+            ) {
+                jogadaClass[0].classList.remove("flip");
+                jogadaClass[1].classList.remove("flip");
+                if (
+                    !jogadaClass[0].classList.contains("flip") &&
+                    !jogadaClass[1].classList.contains("flip")
+                ) {
+                    jogadaClass[0].classList.remove("jogada");
+                    jogadaClass[1].classList.remove("jogada");
+                }
+            }
+
+            jogadas = [];
+        }, 2000);
+    }
+    console.log(pontuacao);
+}
+// escolhe 1 carta, escolhe a segunda e compara com a primeira
+// se for igual permanece virada, e aumenta 1 ponto
+// se for diferente as duas viram e tenta novamente
+// caso todas estiverem virada é fim de jogo
+
 function viraCarta(virar) {
     virar.classList.add("flip");
+    virar.classList.add("jogada");
+
+    regrasDoJogo(virar);
 }
 
 escolheDificuldade();
