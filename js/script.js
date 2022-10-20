314f06a35d2c101aa2bd45aa7ca04565a27fd60b
Victor Motta <victor.mottag@gmail.com>
2022-10-20 02:39:42 -0300

    Adicionado função de escolher quantidade de cartas, adicionar e sortear carta, e carregar o jogo

--- js/script.js
+++ js/script.js
@@ -1,15 +1,75 @@
-const primeiraCarta = document.querySelector(".card-bobrossparrot");
-const segundaCarta = document.querySelector(".card-explodyparrot");
-const terceiraCarta = document.querySelector(".card-fiestaparrot");
-const quartaCarta = document.querySelector(".card-metalparrot");
-const quintaCarta = document.querySelector(".card-revertitparrot");
-const sextaCarta = document.querySelector(".card-tripletsparrot");
-const setimaCarta = document.querySelector(".card-unicornparrot");
-let quantidadeCarta;
+let personagens = [
+    "explodyparrot",
+    "bobrossparrot",
+    "fiestaparrot",
+    "metalparrot",
+    "revertitparrot",
+    "tripletsparrot",
+    "unicornparrot",
+];
 
-console.log(primeiraCarta);
-function embaralhar() {}
+let carta = [];
+let cartasDuplicadas = [];
+let qtdCartas = 0;
+
+function escolheDificuldade() {
+    let condition = true;
+    while (condition) {
+        let escolha = prompt(
+            "Escolha a quantidade de cartas que quer jogar(escolha entre 4 e 14 e somente números pares): "
+        );
+        let par = escolha % 2;
+        if (par == 0 && escolha >= 4 && escolha <= 14) {
+            qtdCartas = escolha / 2;
+            condition = false;
+            console.log(qtdCartas);
+            adicionaCartas();
+            loadGame();
+        } else {
+            alert("Escolha um número entre 4 e 14 e somente número par: ");
+            condition = true;
+        }
+    }
+}
+
+function criaCartas(caracter) {
+    const elemento = document.querySelector(".game");
+
+    elemento.innerHTML += `
+    <div id="${caracter}" onclick="viraCarta(this);" class="card">
+        <div class="front-face face">
+            <img src="./img/${caracter}.gif" alt="" />
+        </div>
+        <div class="back-face face ">
+            <img src="./img/back.png" alt="" />
+        </div>
+    </div>
+    `;
+}
+
+function adicionaCartas() {
+    for (let i = 0; i < qtdCartas; i++) {
+        carta.push(personagens[i]);
+    }
+}
+
+function sorteiaCartas() {
+    cartasDuplicadas = [...carta, ...carta];
+    const cartasSorteadas = cartasDuplicadas.sort((a, b) => Math.random() - 0.5);
+
+    return cartasSorteadas;
+}
+
+function loadGame() {
+    const cartasSorteadas = sorteiaCartas();
+
+    cartasSorteadas.forEach((caracter) => {
+        criaCartas(caracter);
+    });
+}
 
 function viraCarta(virar) {
     virar.classList.add("flip");
 }
+
+escolheDificuldade();
